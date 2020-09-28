15dc23ea69f6712df884734c7a63a420bc0fdb0c
Hannah Niemi <hnniemi@umich.edu>
2020-09-28 19:41:24 -0400

    updates

--- main.py
+++ main.py
@@ -3,46 +3,24 @@ import pandas as pd
 import numpy as np
 from flask import Flask, render_template, request, redirect, url_for, flash, send_from_directory
 from werkzeug.utils import secure_filename
-
-UPLOAD_FOLDER = os.path.dirname(os.path.realpath(__file__)) + '\\Uploads'
-ALLOWED_EXTENSIONS = {'txt', 'csv'}
+from os.path import join, dirname, realpath
 
 app = Flask(__name__)
-app.secret_key = b'19IfP1zSZL'
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
-
 
 @app.route('/', methods=['GET', 'POST'])
-def home():
+def index():
+    return render_template('index.html')
+
+
+@app.route('/data', methods=['GET', 'POST'])
+def data():
     if request.method == 'POST':
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
+        file = request.form['upload-file']
+        data = pd.read_csv(file)
+        summary_stats = data.describe()
+        ss_dict = summary_stats.to_dict()
+        return render_template('data.html', data=data.to_html(), summary_data=summary_stats.to_html())
 
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('uploaded_file',
-                                    filename=filename))
-    return render_template('home.html')
-
-
-@app.route('/uploads/<filename>')
-def uploaded_file(filename):
-    file = open(UPLOAD_FOLDER + '\\' + filename, "r")
-    df = pd.read_csv(file)
-    return df.describe()
-
-
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
-
+  
 if __name__ == '__main__':
     app.run(debug=True)
